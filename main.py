0d7d485283afe72e7fb16419e29894e4b4a708cc
dasha <dpaseka7@gmail.com>
2024-10-21 19:17:09 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-def is_glsn(char):
+def glsn(char):
     glsn = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"
     return char in glsn
-def is_sogl(char):
+def sogl(char):
     sogl = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ"
     return char in sogl
 user_input = input("Введите строку на русском языке: ")
 glsn_count = 0
 sogl_count = 0
 for char in user_input:
-    if is_glsn(char):
+    if glsn(char):
         glsn_count += 1
-    elif is_sogl(char):
+    elif sogl(char):
         sogl_count += 1
 print(f"Длина строки: {len(user_input)}")
 print(f"Количество гласных: {glsn_count}")
